9d8c8d1b1ab81d1bf5980e96c825b11f2a61ac30
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-03-18 20:52:24 -0400

    testing tests and deploys with cdk

--- go-micah-dev_test.go
+++ go-micah-dev_test.go
@@ -1,26 +1,24 @@
 package main
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/aws/aws-cdk-go/awscdk/v2"
-// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-// 	"github.com/aws/jsii-runtime-go"
-// )
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	"github.com/aws/jsii-runtime-go"
+)
 
-// example tests. To run these tests, uncomment this file along with the
-// example resource in go-micah-dev-cdk_test.go
-// func TestGoMicahDevCdkStack(t *testing.T) {
-// 	// GIVEN
-// 	app := awscdk.NewApp(nil)
+func TestGoMicahDevStack(t *testing.T) {
+	// GIVEN
+	app := awscdk.NewApp(nil)
 
-// 	// WHEN
-// 	stack := NewGoMicahDevCdkStack(app, "MyStack", nil)
+	// WHEN
+	stack := NewGoMicahDevStack(app, "MyStack", nil)
 
-// 	// THEN
-// 	template := assertions.Template_FromStack(stack)
+	// THEN
+	template := assertions.Template_FromStack(stack, nil)
 
-// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-// 		"VisibilityTimeout": 300,
-// 	})
-// }
+	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+		"VisibilityTimeout": 300,
+	})
+}
